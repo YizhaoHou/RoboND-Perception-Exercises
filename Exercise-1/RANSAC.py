597a9ad3b14318b9421dfe017b6dc7760d51d330
YZ <yzhou1612@gmail.com>
2024-09-04 15:47:41 +0800

    e1

--- Exercise-1/RANSAC.py
+++ Exercise-1/RANSAC.py
@@ -6,8 +6,10 @@ cloud = pcl.load_XYZRGB('tabletop.pcd')
 
 
 # Voxel Grid filter
-
-
+voxel_filter = cloud.make_voxel_grid_filter()
+LEAF_SIZE = 0.01 
+voxel_filter.set_leaf_size(LEAF_SIZE, LEAF_SIZE, LEAF_SIZE)
+cloud_filtered = voxel_filter.filter()
 # PassThrough filter
 
 
